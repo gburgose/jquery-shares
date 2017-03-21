1e1c6ccc514a73bb46258f68da4a75bec21b5304
gabriel Burgos <gburgos@digitalmeat.cl>
2017-03-21 14:44:43 -0300

    update plugin

--- resources/plugin/js/main.js
+++ resources/plugin/js/main.js
@@ -11,32 +11,32 @@
 			return window.open(url, "Compartir", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
 		}
 
-		var __social_share = function (e){
+		var __trigger = function (e){
 
 			e.preventDefault();
 
 			var $object = $(this);
 
-			var share_url = encodeURIComponent($object.attr("href"));
-			var share_text = encodeURIComponent($object.attr("data-text"));
+			var data_href  = encodeURIComponent($object.attr("href"));
+			var data_text  = encodeURIComponent($object.attr("data-text"));
 		
 			if ( $object.hasClass('facebook') ){
-					var url = "https://www.facebook.com/sharer/sharer.php?u="+share_url;
+				var url = "https://www.facebook.com/sharer/sharer.php?u="+data_href;
 			} else if ( $object.hasClass('twitter') ){
-					var url = "https://twitter.com/intent/tweet?source=webclient&amp;text="+share_text+"&amp;url="+share_url;
-						} else if ( $object.hasClass('linkedin') ){
-									var url = "https://www.linkedin.com/shareArticle?mini=true&url="+share_url+"&title="+share_text
-						} else if ( $object.hasClass('google-plus') ){
-									var url = "https://plus.google.com/share?url="+share_url
-				} else {
-					return true;
+				var url = "https://twitter.com/intent/tweet?source=webclient&amp;text="+data_text+"&amp;url="+data_href;
+			} else if ( $object.hasClass('linkedin') ){
+				var url = "https://www.linkedin.com/shareArticle?mini=true&url="+data_href+"&title="+data_text
+			} else if ( $object.hasClass('google-plus') ){
+				var url = "https://plus.google.com/share?url="+data_href
+			} else {
+				return false;
 			}
 
 			__popupWindow(url, 500, 310);
 		
 		}
 
-		$(this).click(__social_share);
+		$(this).click(__trigger);
 
  }; 
 
