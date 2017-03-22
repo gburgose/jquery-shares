2ae6c7dcb5b742965dc203963f7bbcb6c007d79a
Gabriel Burgos <hola@gabrielburgos.cl>
2017-03-21 23:51:56 -0300

    shares

--- dist/jquery.shares.js
+++ dist/jquery.shares.js
@@ -2,41 +2,49 @@
 
   'use strict';
 
-  $.fn.share = function() {
+  $.fn.shares = function() {
 
-    var __social_share = function(e) {
+    var constructor = function($anchor) {
 
-      e.preventDefault();
-
-      var $object = $(this);
-
-      var share_url = encodeURIComponent($object.attr("href"));
-      var share_text = encodeURIComponent($object.attr("data-text"));
-
-      if ($object.hasClass('facebook')) {
-        var url = "https://www.facebook.com/sharer/sharer.php?u=" + share_url;
-      } else if ($object.hasClass('twitter')) {
-        var url = "https://twitter.com/intent/tweet?source=webclient&amp;text=" + share_text + "&amp;url=" + share_url;
-      } else if ($object.hasClass('linkedin')) {
-        var url = "https://www.linkedin.com/shareArticle?mini=true&url=" + share_url + "&title=" + share_text
-      } else if ($object.hasClass('google-plus')) {
-        var url = "https://plus.google.com/share?url=" + share_url
-      } else {
-        return true;
+      var __popupWindow = function(url, w, h) {
+        var left, top;
+        left = ($(window).width() / 2) - (w / 2);
+        top = ($(window).height() / 2) - (h / 2);
+        return window.open(url, "Compartir", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
       }
 
-      __popupWindow(url, 500, 310);
+      var __trigger = function(e) {
+
+        e.preventDefault();
+
+        var $object = $(this);
+
+        var data_href = encodeURIComponent($object.attr("href"));
+        var data_text = encodeURIComponent($object.attr("data-text"));
+
+        if ($object.hasClass('facebook')) {
+          var url = "https://www.facebook.com/sharer/sharer.php?u=" + data_href;
+        } else if ($object.hasClass('twitter')) {
+          var url = "https://twitter.com/intent/tweet?source=webclient&amp;text=" + data_text + "&amp;url=" + data_href;
+        } else if ($object.hasClass('linkedin')) {
+          var url = "https://www.linkedin.com/shareArticle?mini=true&url=" + data_href + "&title=" + data_text
+        } else if ($object.hasClass('google-plus')) {
+          var url = "https://plus.google.com/share?url=" + data_href
+        } else {
+          return false;
+        }
+
+        __popupWindow(url, 500, 310);
+
+      }
+
+      $anchor.bind("click", __trigger);
 
     }
 
-    __popupWindow = function(url, w, h) {
-      var left, top;
-      left = ($(window).width() / 2) - (w / 2);
-      top = ($(window).height() / 2) - (h / 2);
-      return window.open(url, "Compartir", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
-    }
-
-    $(this).click(__social_share);
+    return this.each(function() {
+      constructor($(this));
+    });
 
   };
 
--- gulpfile.js
+++ gulpfile.js
@@ -7,43 +7,25 @@
 |
 */
 
-var gulp   = require('gulp'),
-  concat   = require('gulp-concat'),
-  uglify   = require('gulp-uglify'),
-  rename   = require("gulp-rename"),
-  addsrc   = require('gulp-add-src'),
-  beautify = require('gulp-beautify');
+var gulp    = require('gulp'),
+	concat    = require('gulp-concat'),
+	uglify    = require('gulp-uglify'),
+	rename    = require("gulp-rename"),
+	addsrc    = require('gulp-add-src'),
+	beautify  = require('gulp-beautify');
 
 gulp.task('default', function() {
-  gulp.src(['./resources/plugin/js/main.js'])
-    // normal
-    .pipe(concat('dist/jquery.formulator.js'))
-    .pipe(beautify({indent_size: 2}))
-    .pipe(gulp.dest('.'))
-    // min
-    .pipe(uglify({ preserveComments: false }))
-    .pipe(rename({ suffix : '.min' }))
-    .pipe(gulp.dest('.'));
+	gulp.src(['./resources/plugin/js/main.js'])
+		// normal
+		.pipe(concat('dist/jquery.shares.js'))
+		.pipe(beautify({indent_size: 2}))
+		.pipe(gulp.dest('.'))
+		// min
+		.pipe(uglify({ preserveComments: false }))
+		.pipe(rename({ suffix : '.min' }))
+		.pipe(gulp.dest('.'));
 });
-
-jsDependecies = [
-  './node_modules/jquery-form/src/jquery.form.js',
-  './node_modules/jquery-validation/dist/jquery.validate.js',
-  './node_modules/jquery-validation/dist/additional-methods.js',
-  './node_modules/jquery.rut/jquery.rut.js'
-];
-
-gulp.task('package', function() {
-  gulp.src(['./resources/plugin/js/main.js'])
-    .pipe(addsrc.prepend( jsDependecies ))
-    .pipe(uglify({ preserveComments: false }))
-    .pipe(rename({ suffix : '.min' }))
-    .pipe(concat('dist/jquery.formulator.pkg.js'))
-    .pipe(gulp.dest('.'));
-});
-
-gulp.task('build',['default', 'package']);
 
 gulp.task('watch', function () {
-  gulp.watch('src/jquery.formulator.js', ['default']);
+	gulp.watch('src/jquery.shares.js', ['default']);
 });
