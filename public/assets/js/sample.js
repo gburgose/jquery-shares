39322a4ca348649efc4f0572cb1a88f4dffa6fab
Gabriel Burgos <hola@gabrielburgos.cl>
2017-03-21 21:51:50 -0300

    v1.1 estable

--- public/assets/js/sample.js
+++ public/assets/js/sample.js
@@ -10355,38 +10355,45 @@ $(document).ready(function () {
 
 	$.fn.shares = function () {
 
-		var __popupWindow = function __popupWindow(url, w, h) {
-			var left, top;
-			left = $(window).width() / 2 - w / 2;
-			top = $(window).height() / 2 - h / 2;
-			return window.open(url, "Compartir", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+		var constructor = function constructor($anchor) {
+
+			var __popupWindow = function __popupWindow(url, w, h) {
+				var left, top;
+				left = $(window).width() / 2 - w / 2;
+				top = $(window).height() / 2 - h / 2;
+				return window.open(url, "Compartir", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+			};
+
+			var __trigger = function __trigger(e) {
+
+				e.preventDefault();
+
+				var $object = $(this);
+
+				var data_href = encodeURIComponent($object.attr("href"));
+				var data_text = encodeURIComponent($object.attr("data-text"));
+
+				if ($object.hasClass('facebook')) {
+					var url = "https://www.facebook.com/sharer/sharer.php?u=" + data_href;
+				} else if ($object.hasClass('twitter')) {
+					var url = "https://twitter.com/intent/tweet?source=webclient&amp;text=" + data_text + "&amp;url=" + data_href;
+				} else if ($object.hasClass('linkedin')) {
+					var url = "https://www.linkedin.com/shareArticle?mini=true&url=" + data_href + "&title=" + data_text;
+				} else if ($object.hasClass('google-plus')) {
+					var url = "https://plus.google.com/share?url=" + data_href;
+				} else {
+					return false;
+				}
+
+				__popupWindow(url, 500, 310);
+			};
+
+			$anchor.bind("click", __trigger);
 		};
 
-		var __trigger = function __trigger(e) {
-
-			e.preventDefault();
-
-			var $object = $(this);
-
-			var data_href = encodeURIComponent($object.attr("href"));
-			var data_text = encodeURIComponent($object.attr("data-text"));
-
-			if ($object.hasClass('facebook')) {
-				var url = "https://www.facebook.com/sharer/sharer.php?u=" + data_href;
-			} else if ($object.hasClass('twitter')) {
-				var url = "https://twitter.com/intent/tweet?source=webclient&amp;text=" + data_text + "&amp;url=" + data_href;
-			} else if ($object.hasClass('linkedin')) {
-				var url = "https://www.linkedin.com/shareArticle?mini=true&url=" + data_href + "&title=" + data_text;
-			} else if ($object.hasClass('google-plus')) {
-				var url = "https://plus.google.com/share?url=" + data_href;
-			} else {
-				return false;
-			}
-
-			__popupWindow(url, 500, 310);
-		};
-
-		$(this).click(__trigger);
+		return this.each(function () {
+			constructor($(this));
+		});
 	};
 })(jQuery);
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
--- resources/plugin/js/main.js
+++ resources/plugin/js/main.js
@@ -4,39 +4,47 @@
 	
 	$.fn.shares = function() {
 
-		var __popupWindow = function (url, w, h) {
-			var left, top;
-			left = ($(window).width() / 2) - (w / 2);
-			top = ($(window).height() / 2) - (h / 2);
-			return window.open(url, "Compartir", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
-		}
+		var constructor = function( $anchor ){
 
-		var __trigger = function (e){
-
-			e.preventDefault();
-
-			var $object = $(this);
-
-			var data_href  = encodeURIComponent($object.attr("href"));
-			var data_text  = encodeURIComponent($object.attr("data-text"));
-		
-			if ( $object.hasClass('facebook') ){
-				var url = "https://www.facebook.com/sharer/sharer.php?u="+data_href;
-			} else if ( $object.hasClass('twitter') ){
-				var url = "https://twitter.com/intent/tweet?source=webclient&amp;text="+data_text+"&amp;url="+data_href;
-			} else if ( $object.hasClass('linkedin') ){
-				var url = "https://www.linkedin.com/shareArticle?mini=true&url="+data_href+"&title="+data_text
-			} else if ( $object.hasClass('google-plus') ){
-				var url = "https://plus.google.com/share?url="+data_href
-			} else {
-				return false;
+			var __popupWindow = function (url, w, h) {
+				var left, top;
+				left = ($(window).width() / 2) - (w / 2);
+				top = ($(window).height() / 2) - (h / 2);
+				return window.open(url, "Compartir", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
 			}
 
-			__popupWindow(url, 500, 310);
-		
+			var __trigger = function (e){
+
+				e.preventDefault();
+
+				var $object = $(this);
+
+				var data_href  = encodeURIComponent($object.attr("href"));
+				var data_text  = encodeURIComponent($object.attr("data-text"));
+			
+				if ( $object.hasClass('facebook') ){
+					var url = "https://www.facebook.com/sharer/sharer.php?u="+data_href;
+				} else if ( $object.hasClass('twitter') ){
+					var url = "https://twitter.com/intent/tweet?source=webclient&amp;text="+data_text+"&amp;url="+data_href;
+				} else if ( $object.hasClass('linkedin') ){
+					var url = "https://www.linkedin.com/shareArticle?mini=true&url="+data_href+"&title="+data_text
+				} else if ( $object.hasClass('google-plus') ){
+					var url = "https://plus.google.com/share?url="+data_href
+				} else {
+					return false;
+				}
+
+				__popupWindow(url, 500, 310);
+			
+			}
+
+			$anchor.bind( "click" , __trigger );
+
 		}
 
-		$(this).click(__trigger);
+		return this.each(function () {
+			constructor( $(this) );
+		});
 
  }; 
 
