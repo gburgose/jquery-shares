34548edb0e7c8b14b7455b7791197c6f0d79cee3
Gabriel Burgos <hola@gabrielburgos.cl>
2017-05-27 21:54:41 -0400

    [Global] update

--- public/index.php
+++ public/index.php
@@ -68,8 +68,5 @@
 
 	</div>
 
-	<script id="__bs_script__">//<![CDATA[document.write("<script async src='/browser-sync/browser-sync-client.js?v=2.18.8'><\/script>".replace("HOST", location.hostname));
-//]]></script>
-
 </body>
 </html>
